15184390a9f93c49f8833223204a69172dba9258
Dickson Law <dicksonlaw583@gmail.com>
2014-07-28 16:04:39 -0400

    Updated schema --- should be reasonably backwards compatible

--- A2/schema.sql
+++ A2/schema.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: Jul 17, 2014 at 02:20 AM
+-- Generation Time: Jul 28, 2014 at 10:02 PM
 -- Server version: 5.5.34
 -- PHP Version: 5.5.10
 
@@ -21,9 +21,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `attends` (
-  `party` int(11) unsigned NOT NULL REFERENCES parties(id),
-  `guest` int(11) unsigned NOT NULL REFERENCES users(id),
-  PRIMARY KEY (`party`,`guest`)
+  `party` int(11) unsigned NOT NULL,
+  `guest` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`party`,`guest`),
+  KEY `attends_ibfk_2` (`guest`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -35,57 +36,86 @@ CREATE TABLE `attends` (
 CREATE TABLE `parties` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(128) NOT NULL,
-  `host` int(11) unsigned NOT NULL REFERENCES users(id),
+  `host` int(11) unsigned NOT NULL,
   `capacity` int(8) NOT NULL,
   `address` varchar(256) NOT NULL,
+  `city` varchar(32) NOT NULL,
+  `province` varchar(32) NOT NULL,
   `latitude` decimal(9,6) NOT NULL,
   `longitude` decimal(9,6) NOT NULL,
   `description` text,
   `posted_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `start_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `end_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' CHECK (end_date > start_date),
-  `ended` tinyint(1) NOT NULL DEFAULT 0,
+  `end_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `ended` tinyint(1) NOT NULL DEFAULT '0',
   `featured_until` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `streaming` tinyint(1) NOT NULL DEFAULT 0,
-  `private` tinyint(1) NOT NULL DEFAULT 0,
-  `food_provided` tinyint(1) NOT NULL DEFAULT 0,
-  `alcohol` tinyint(1) NOT NULL DEFAULT 0,
-  `parking` tinyint(1) NOT NULL DEFAULT 0,
-  `adult_only` tinyint(1) NOT NULL DEFAULT 0,
+  `streaming` tinyint(1) NOT NULL DEFAULT '0',
+  `private` tinyint(1) NOT NULL DEFAULT '0',
+  `food_provided` tinyint(1) NOT NULL DEFAULT '0',
+  `alcohol` tinyint(1) NOT NULL DEFAULT '0',
+  `parking` tinyint(1) NOT NULL DEFAULT '0',
+  `adult_only` tinyint(1) NOT NULL DEFAULT '0',
+  `rating` decimal(3,2) NOT NULL DEFAULT '0.00',
+  `rating_count` int(11) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Triggers `parties`
+--
+DROP TRIGGER IF EXISTS `update_host_rating`;
+DELIMITER //
+CREATE TRIGGER `update_host_rating` AFTER UPDATE ON `parties`
+ FOR EACH ROW BEGIN
+
+IF old.rating <> new.rating THEN
+UPDATE users SET rating = (SELECT AVG(rating) FROM parties WHERE host = new.host), rating_count = (SELECT SUM(rating_count) FROM parties WHERE host = new.host) WHERE users.id = new.host;
+END IF;
+
+END
+//
+DELIMITER ;
+
 -- --------------------------------------------------------
 
-CREATE TABLE `uploads` (
-	`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-	`posted_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`owner` int(11) unsigned NOT NULL REFERENCES users(id),
-	PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
-
-CREATE TABLE `user_album` (
-	`owner` int(11) unsigned NOT NULL REFERENCES users(id),
-	`picture` int(11) unsigned NOT NULL REFERENCES uploads(id),
-	PRIMARY KEY (`owner`, `picture`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1; 
+--
+-- Table structure for table `party_album`
+--
 
 CREATE TABLE `party_album` (
-	`owner` int(11) unsigned NOT NULL REFERENCES parties(id),
-	`picture` int(11) unsigned NOT NULL REFERENCES uploads(id),
-	PRIMARY KEY (`owner`, `picture`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1; 
+  `owner` int(11) unsigned NOT NULL,
+  `picture` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`owner`,`picture`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `ratings`
 --
 
 CREATE TABLE `ratings` (
-  `rated_by` int(11) unsigned NOT NULL REFERENCES users(id),
-  `rated_for` int(11) unsigned NOT NULL REFERENCES parties(id),
+  `rated_by` int(11) unsigned NOT NULL,
+  `rated_for` int(11) unsigned NOT NULL,
   `rating` tinyint(4) unsigned NOT NULL,
   `comment` varchar(256) DEFAULT NULL,
-  PRIMARY KEY (`rated_by`,`rated_for`)
+  PRIMARY KEY (`rated_by`,`rated_for`),
+  KEY `ratings_ibfk_2` (`rated_for`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Triggers `ratings`
+--
+DROP TRIGGER IF EXISTS `update_ratings_remote`;
+DELIMITER //
+CREATE TRIGGER `update_ratings_remote` AFTER INSERT ON `ratings`
+ FOR EACH ROW BEGIN
+
+UPDATE parties SET rating = (SELECT AVG(rating) FROM ratings WHERE ratings.rated_for = new.rated_for), rating_count = (SELECT COUNT(*) FROM ratings WHERE ratings.rated_for = new.rated_for) WHERE parties.id = new.rated_for;
+
+END
+//
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -94,10 +124,25 @@ CREATE TABLE `ratings` (
 --
 
 CREATE TABLE `subscribes_to` (
-  `subscriber` int(11) unsigned NOT NULL REFERENCES users(id),
-  `user` int(11) unsigned NOT NULL REFERENCES users(id),
-  PRIMARY KEY (`subscriber`,`user`)
+  `subscriber` int(11) unsigned NOT NULL,
+  `user` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`subscriber`,`user`),
+  KEY `subscribes_to_ibfk_2` (`user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `uploads`
+--
+
+CREATE TABLE `uploads` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `posted_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `owner` int(11) unsigned NOT NULL,
+  `extension` varchar(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -108,13 +153,32 @@ CREATE TABLE `subscribes_to` (
 CREATE TABLE `users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(64) NOT NULL,
+  `first_name` varchar(16) NOT NULL,
+  `last_name` varchar(32) NOT NULL,
+  `email` varchar(32) NOT NULL,
   `password` varchar(128) NOT NULL,
   `join_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `address` varchar (256) NOT NULL,
+  `address` varchar(256) NOT NULL,
+  `city` varchar(32) NOT NULL,
+  `province` varchar(32) NOT NULL,
   `latitude` decimal(9,6) NOT NULL,
   `longitude` decimal(9,6) NOT NULL,
+  `rating` decimal(3,2) NOT NULL DEFAULT '0.00',
+  `rating_count` int(11) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_album`
+--
+
+CREATE TABLE `user_album` (
+  `owner` int(11) unsigned NOT NULL,
+  `picture` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`owner`,`picture`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Constraints for dumped tables
