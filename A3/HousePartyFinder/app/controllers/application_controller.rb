cb7169edb6389e5da0e34e9b3efc89a97e90fc33
Dickson Law <dicksonlaw583@gmail.com>
2014-07-30 19:01:20 -0400

    Added temporary homepage

--- A3/HousePartyFinder/app/controllers/application_controller.rb
+++ A3/HousePartyFinder/app/controllers/application_controller.rb
@@ -4,11 +4,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, :if => :devise_controller?
 
+  def page_title(title)
+    @page_title = title
+  end
+
   protected
 
   def configure_permitted_parameters
   	devise_parameter_sanitizer.for(:sign_up) { |u|
-  		u.permit(:username, :email, :password, :password_confirmation);
+  		u.permit(:username, :email, :password, :password_confirmation)
   	}
   end
 
