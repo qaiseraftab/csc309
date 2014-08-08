dd8f3ed6ef9f993cc7d6697e21615407e8e7495c
Dickson Law <dicksonlaw583@gmail.com>
2014-08-07 21:21:03 -0400

    Added migration for removing botched default value for featured_until in users

--- A3/HousePartyFinder/db/schema.rb
+++ A3/HousePartyFinder/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140807223639) do
+ActiveRecord::Schema.define(version: 20140808011804) do
 
   create_table "album_attachments", force: true do |t|
     t.string   "access_token"
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 20140807223639) do
     t.datetime "start_date"
     t.datetime "end_date"
     t.boolean  "ended",               default: false
-    t.datetime "featured_until",      default: '2014-08-07 00:00:00'
+    t.datetime "featured_until"
     t.boolean  "streaming"
     t.boolean  "private"
     t.boolean  "food_provided"
