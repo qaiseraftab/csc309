330d854a083dde634605b6edadff5d8a399c2794
Dickson Law <dicksonlaw583@gmail.com>
2014-08-07 20:28:09 -0400

    Updated seed admin user to be validation-compatible

--- A3/HousePartyFinder/db/seeds.rb
+++ A3/HousePartyFinder/db/seeds.rb
@@ -1,1 +1,13 @@
-User.create!(:email => "admin@housepartyfinder.com", :username => "admin", :admin => "t", :password => "admin123", :address => "58 Page Avenue", :city => "toronto", :province => "Ontario")
+User.create!(
+	:email => "admin@housepartyfinder.com", 
+	:username => "admin", 
+	:first_name => "Admin",
+	:last_name => "User",
+	:admin => true,
+	:password => "admin123",
+	:address => "58 Page Avenue",
+	:city => "Toronto", 
+	:province => "Ontario",
+	:latitude => 43.7835492,
+	:longitude => -79.3832311
+)
